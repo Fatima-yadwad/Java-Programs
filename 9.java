5076312f3592365691bef2ca8781ffe489f451f5
Fatima-yadwad <bibifatimayadwad@gmail.com>
2026-06-24 12:20:04 +0530

    Added Java OOP programs

---
+++ 9.java
@@ -0,0 +1,30 @@
+class Employee {
+    void calculateSalary() {
+        System.out.println("Employee Salary");
+    }
+}
+
+class FullTimeEmployee extends Employee {
+    void calculateSalary() {
+        System.out.println("Salary = Rs.50000");
+    }
+}
+
+class PartTimeEmployee extends Employee {
+    void calculateSalary() {
+        System.out.println("Salary = Rs.15000");
+    }
+
+}
+
+    public class 9{
+
+    public static void main(String[] args) {
+
+        FullTimeEmployee f = new FullTimeEmployee();
+        PartTimeEmployee p = new PartTimeEmployee();
+
+        f.calculateSalary();
+        p.calculateSalary();
+    }
+}
